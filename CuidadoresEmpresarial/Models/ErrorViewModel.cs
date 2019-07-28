45f48cd0ab4fa017eba82d1d2cc0284771755cf6
Edivaldo Bispo das Chagas <edivaldo.b.chagas@outlook.com>
2019-07-27 23:40:21 -0300

    Primeiro Versionamento do meu projeto localmente

---
+++ CuidadoresEmpresarial/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CuidadoresEmpresarial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
